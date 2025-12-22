acf9e29923dc523eeddf02111cf8aafd5a6d2184
Nahuel Whittall <neyennahuel@gmail.com>
2025-12-22 11:07:29 -0300

    a

--- script.js
+++ script.js
@@ -47,9 +47,6 @@ function isValidUrl(value) {
 function renderMenu(items) {
     items.forEach(item => {
 
-        // 👇 LOG CLAVE PARA DIAGNÓSTICO
-        console.log("IMAGEN LEÍDA:", item.Imagen);
-
         if (item.Disponible !== "TRUE") return;
 
         const categoriaId = normalize(item.Categoria);
